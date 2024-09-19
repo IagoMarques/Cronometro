e9df42b4d40fa3b12e7c8cbbc7fbf5707576224d
Iago <iagofariasmarques@gmail.com>
2024-09-19 17:38:13 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Timer from './assets/components/Timer';
+
+function App() {
+
+  return <></>;
+  
+}
+
+export default App;
---
+++ src/assets/components/Timer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Timer = () => {
+    return <div className="timer-container">
+        <Laplist/>    
+    </div>;
+};
+
+export default Timer;
