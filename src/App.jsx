f45f074c6eef7454331adfa28d673295b31dfa13
Iago <iagofariasmarques@gmail.com>
2024-09-23 18:34:53 -0300

    Esqueci um parêntese

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import Timer from "./assets/components/Timer";
 
 function App() {
-  return;
-  <>
-    <Timer />
-  </>;
+  return (
+    <>
+      <Timer />
+    </>
+  );
 }
 
 export default App;
